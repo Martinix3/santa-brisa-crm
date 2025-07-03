aff63cf87f03b609eac0e5dd4207755bc5a6461a
Martin Jaime <mj@santabrisa.co>
2025-07-03 10:21:57 +0000

    por favor, arregla el logo, que no se ve.

--- next.config.ts
+++ next.config.ts
@@ -27,7 +27,7 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
         port: '',
-        pathname: '/**',
+        pathname: '/v0/b/santa-brisa-crm.appspot.com/o/**',
       },
     ],
   },

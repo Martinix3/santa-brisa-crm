2c189bab5217a717fd06f3d33ea19e83c8fda8f2
Martin Jaime <mj@santabrisa.co>
2025-07-04 17:38:23 +0000

    por favor, parece que ahora funciona, pero recuerda que teníamos que pon

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,12 +1,9 @@
-
-import {genkit} from 'genkit';
-import {googleAI, gemini15Flash} from '@genkit-ai/googleai';
+import { gemini15Pro, googleAI } from '@genkit-ai/googleai';
+import { genkit } from 'genkit';
 
 // Configure a Genkit instance to use the Google AI plugin.
 // It will automatically use the GOOGLE_API_KEY from the .env file.
 export const ai = genkit({
-  plugins: [
-    googleAI(),
-  ],
-  model: gemini15Flash, // Set default model
+  plugins: [googleAI()],
+  model: gemini15Pro, // Set default model to the more powerful preview version
 });

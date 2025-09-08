a78df1f5b70ce7312fcafb50b724856efa8aaa68
Martin Jaime <mj@santabrisa.co>
2025-09-08 17:02:20 +0000

    Try fixing this error: `Console Error: In HTML, <button> cannot be a des

--- src/components/ui/multi-select.tsx
+++ src/components/ui/multi-select.tsx
@@ -60,22 +60,30 @@ function MultiSelect({ options, selected, onChange, className, disabled, ...prop
                     }}
                   >
                     {option.label}
-                    <button
+                    <span
+                        role="button"
+                        tabIndex={0}
                         aria-label={`Remove ${option.label} option`}
                         className="ml-1 ring-offset-background rounded-full outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2"
                         onKeyDown={(e) => {
-                            if (e.key === "Enter") {
-                            handleUnselect(option.value);
+                            if (e.key === "Enter" || e.key === " ") {
+                              e.preventDefault();
+                              e.stopPropagation();
+                              handleUnselect(option.value);
                             }
                         }}
                         onMouseDown={(e) => {
                             e.preventDefault();
                             e.stopPropagation();
                         }}
-                        onClick={() => handleUnselect(option.value)}
+                        onClick={(e) => {
+                            e.preventDefault();
+                            e.stopPropagation();
+                            handleUnselect(option.value)
+                        }}
                     >
                         <X className="h-3 w-3 text-muted-foreground hover:text-foreground" />
-                    </button>
+                    </span>
                   </Badge>
                 ))
             ) : (

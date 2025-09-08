7ffe55706c941b50085ee38340f9054f7ae931a8
Martin Jaime <mj@santabrisa.co>
2025-09-08 10:41:10 +0000

    agenda-actions.ts:48  POST https://9000-firebase-studio-1749550815469.cl

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 
 module.exports = {
-  allowedDevOrigins: ['.cloudworkstations.dev','*.cloudworkstations.dev', 'firebase-studio-1749550815469.cluster-6vyo4gb53jczovun3dxslzjahs.cloudworkstations.dev'],
+  experimental: {
+    serverActions: {
+      allowedForwardedHosts: ['firebase-studio-1749550815469.cluster-6vyo4gb53jczovun3dxslzjahs.cloudworkstations.dev'],
+      allowedOrigins: ['9000-firebase-studio-1749550815469.cluster-6vyo4gb53jczovun3dxslzjahs.cloudworkstations.dev'],
+    },
+  },
 };

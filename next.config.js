c168517bcf4d9a7f236979d2573237fca0c69212
Martin Jaime <mj@santabrisa.co>
2025-09-08 11:51:54 +0000

    ⚠ Invalid next.config.js options detected:  ⚠     Unrecognized key(s) i

--- next.config.js
+++ next.config.js
@@ -2,10 +2,8 @@
 module.exports = {
   experimental: {
     serverActions: {
-      allowedForwardedHosts: ['firebase-studio-1749550815469.cluster-6vyo4gb53jczovun3dxslzjahs.cloudworkstations.dev'],
-      allowedOrigins: ['9000-firebase-studio-1749550815469.cluster-6vyo4gb53jczovun3dxslzjahs.cloudworkstations.dev'],
+      allowedForwardedHosts: ['firebase-studio-1749550815469.cluster-6vyo4gb53jczovun3dxslzjahs.cloudworkstations.dev', 'localhost'],
+      allowedOrigins: ['9000-firebase-studio-1749550815469.cluster-6vyo4gb53jczovun3dxslzjahs.cloudworkstations.dev', 'localhost:3000'],
     },
-    // The `allowedDevOrigins` key should be directly under experimental, not inside serverActions.
-    allowedDevOrigins: ["*.cloudworkstations.dev"],
   },
 };

3c61ffe3f79d8cb1d9b22336db33c14190f09f13
Martin Jaime <mj@santabrisa.co>
2025-09-09 15:06:03 +0000

    Sí: puedes hacer que **gcloud** y tu app usen siempre la cuenta de servi

--- next.config.js
+++ next.config.js
@@ -1,1 +1,7 @@
-;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Aquí se pueden añadir futuras configuraciones de Next.js si son necesarias.
+  // Por ahora, una configuración vacía pero bien formada es suficiente.
+};
+
+module.exports = nextConfig;
